bd46dfafabd75aead42f6d49f91fb4e4b4bcaa07
Adam Davis <davisadamw@gmail.com>
2020-06-17 16:47:06 -0700

    get number of stations accessible in each block group, attach to spatial data for mapping

---
+++ FCEV_03_bg-access-summary.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(sf)
+
+# distance threshold ~ less than half of vehicle range
+d_thresh <- 312/2*0.9*1608
+
+distances <- read_rds("Data/all_station_bg_dists.rds") %>% 
+  filter(d_meters < d_thresh)
+
+# block group # of stations in range
+bg_access <- distances %>% 
+  with_groups(T_GEOID, summarize, stations = sum(stations))
+
+bg_locations <- read_rds("~/GIS DataBase/CA_land_only/area-land-area-water/out_dataset/ca_bgs_nowater.rds")
+
+# attach info to block groups and rewrite for mapping
+bg_locations %>% 
+  left_join(bg_access, by = c("GEOID" = "T_GEOID")) %>% 
+  write_sf("Data/Spatial/bg_totals.shp")
