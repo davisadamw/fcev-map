a802b241e333c78443439c998cd399de579c7e3e
Adam Davis <davisadamw@gmail.com>
2020-06-27 16:13:53 -0700

    also check for stations near home and ignore location matched places that are too remote

--- FCEV_03_bg-access-summary.R
+++ FCEV_03_bg-access-summary.R
@@ -8,7 +8,10 @@ distances <- read_rds("Data/all_station_bg_dists.rds")
 
 # block group # of stations in range ... note: only geoid with two stations has two stations in same place
 bg_access <- distances %>%
-  with_groups(T_GEOID, summarize, stations = sum(d_meters < d_thresh))
+  with_groups(T_GEOID, summarize, 
+              any_05mi = any(d_meters < 5 * 1608, na.rm = TRUE),
+              any_10mi = any(d_meters < 10 * 1608, na.rm = TRUE),
+              stations = sum(d_meters < d_thresh, na.rm = TRUE))
 
 # now for the spatial part
 bg_locations <- read_rds("~/GIS DataBase/CA_land_only/area-land-area-water/out_dataset/ca_bgs_nowater.rds")
@@ -23,7 +26,8 @@ intpts <- bg_locations %>%
   st_as_sf(coords = c("INTPTLON", "INTPTLAT"), crs = 4269) %>% 
   st_transform(3310) %>% 
   bind_cols(as_tibble(st_coordinates(.))) %>% 
-  st_drop_geometry()
+  st_drop_geometry() %>% 
+  as_tibble()
 
 # id bad locations
 bg_locations_bad  <- anti_join(intpts, bg_access, by = c("GEOID" = "T_GEOID"))
@@ -33,17 +37,21 @@ bg_locations_good <- anti_join(intpts, bg_locations_bad, by = "GEOID")
 nearest_good_geoid <- RANN::nn2(select(bg_locations_good, X, Y),
                                 select(bg_locations_bad,  X, Y),
                                 k = 1) %>% 
-  pluck("nn.idx") %>% 
-  as.vector()
+  map(~ .[, 1]) %>% 
+  as_tibble()
 
+# don't keep nearest neighbor match if it's at least 10km from its neighbor
 bg_bad_matched <- bg_locations_bad %>% 
-  mutate(near_geoid = bg_locations_good$GEOID[nearest_good_geoid]) %>% 
+  bind_cols(nearest_good_geoid) %>% 
+  mutate(near_geoid = if_else(nn.dists < 20000,
+                              bg_locations_good$GEOID[nn.idx],
+                              NA_character_)) %>% 
   left_join(bg_access, c("near_geoid" = "T_GEOID")) %>% 
-  select(T_GEOID = GEOID, stations)
+  select(T_GEOID = GEOID, stations, any_05mi, any_10mi) %>% 
+  mutate(across(-T_GEOID, replace_na, 0))
 
 # all block groups
 bg_locations %>% 
   select(GEOID, county, geometry) %>% 
   left_join(bind_rows(bg_access, bg_bad_matched), by = c("GEOID" = "T_GEOID")) %>%
-  mutate(stations_na0 = if_else(stations == 0, NA_integer_, stations)) %>% 
   write_sf("Data/Spatial/bg_totals.shp")
