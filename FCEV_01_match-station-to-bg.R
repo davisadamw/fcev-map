5f04da66fc13ba6fa147158ccc48aca530471864
Adam Davis <davisadamw@gmail.com>
2021-09-20 18:01:34 -0700

    update to use new stations

--- FCEV_01_match-station-to-bg.R
+++ FCEV_01_match-station-to-bg.R
@@ -1,16 +1,30 @@
 library(tidyverse)
+library(sf)
 
 # load stations
-stations <- read_rds("Data/stations_sf.rds") %>% 
-  sf::st_drop_geometry()
+stations <- read_csv("Data/stations_raw_20210920.csv",
+                     col_types = cols_only(`Station Name` = col_character(),
+                                           `Open Date`    = col_date(),
+                                           Latitude = col_number(),
+                                           Longitude = col_number()),
+                     lazy = FALSE) %>% 
+  janitor::clean_names(case = "snake") %>% 
+  rename(name = station_name)
+
+# reproject station points
+stations_XY <- stations %>% 
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326) %>% 
+  st_transform(3310) %>% 
+  bind_cols(as_tibble(st_coordinates(.))) %>% 
+  st_drop_geometry()
 
 # load block groups with positions
 bg_pos <- read_rds("Data/ca_bg_seg_position.rds")
 
 # get nearest neighbor
-neighbors <- RANN::nn2(select(bg_pos, X_bgcent, Y_bgcent), select(stations, X, Y), k = 1)
+neighbors <- RANN::nn2(select(bg_pos, X_bgcent, Y_bgcent), select(stations_XY, X, Y), k = 1)
 
-stations_bg <- stations %>% 
+stations_bg <- stations_XY %>% 
   mutate(nearest_bg = bg_pos$GEOID[neighbors$nn.idx])
 
 stations_bg %>% 
--- FCEV_03_bg-access-summary.R
+++ FCEV_03_bg-access-summary.R
@@ -9,9 +9,24 @@ distances <- read_rds("Data/all_station_bg_dists.rds")
 # block group # of stations in range ... note: only geoid with two stations has two stations in same place
 bg_access <- distances %>%
   with_groups(T_GEOID, summarize, 
-              any_05mi = any(d_meters < 5 * 1608, na.rm = TRUE),
-              any_10mi = any(d_meters < 10 * 1608, na.rm = TRUE),
-              stations = sum(d_meters < d_thresh, na.rm = TRUE))
+              nearest_sta_miles = min(d_meters) / 1608,
+              second_nearest_sta_miles = (sort(d_meters)[2]) / 1608,
+              stas_05mi = sum(d_meters < 5 * 1608, na.rm = TRUE),
+              stas_10mi = sum(d_meters < 10 * 1608, na.rm = TRUE),
+              stas_20mi = sum(d_meters < 20 * 1608, na.rm = TRUE),
+              stas_55mi = sum(d_meters < 55 * 1608, na.rm = TRUE),
+              stations = sum(d_meters < d_thresh, na.rm = TRUE)) %>% 
+  mutate(bg_sta1d = case_when(stas_05mi >= 1 ~ "Nearest station within 5 miles",
+                              stas_10mi >= 1 ~ "Nearest station 5-10 miles away",
+                              stas_20mi >= 1 ~ "Nearest station 10-20 miles away",
+                              stas_55mi >= 1 ~ "Nearest station 20-55 miles away",
+                              TRUE ~ "No stations within 55 miles"),
+         bg_sta2d = case_when(stas_05mi >= 2 ~ "Second-nearest station within 5 miles",
+                              stas_10mi >= 2 ~ "Second-nearest station 5-10 miles away",
+                              stas_20mi >= 2 ~ "Second-nearest station 10-20 miles away",
+                              stas_55mi >= 2 ~ "Second-nearest station 20-55 miles away",
+                              stas_55mi >= 1 ~ "One station within 55 miles",
+                              TRUE ~ "No stations within 55 miles"))
 
 # now for the spatial part
 bg_locations <- read_rds("~/GIS DataBase/CA_land_only/area-land-area-water/out_dataset/ca_bgs_nowater.rds")
@@ -47,11 +62,34 @@ bg_bad_matched <- bg_locations_bad %>%
                               bg_locations_good$GEOID[nn.idx],
                               NA_character_)) %>% 
   left_join(bg_access, c("near_geoid" = "T_GEOID")) %>% 
-  select(T_GEOID = GEOID, stations, any_05mi, any_10mi) %>% 
+  select(T_GEOID = GEOID, stations, bg_sta1d, bg_sta2d) %>% 
   mutate(across(-T_GEOID, replace_na, 0))
 
+# add DAC info /// just grab the target tracts
+dac_criteria <- readxl::read_excel("Data/sb535dacsces32018update.xlsx",
+                                   sheet = "SB535 list (2018 update)",
+                                   col_types = "text") %>% 
+  select(GEOID_tr   = `Census Tract`,
+         dac_status = 3)
+
+
 # all block groups
-bg_locations %>% 
-  select(GEOID, county, geometry) %>% 
-  left_join(bind_rows(bg_access, bg_bad_matched), by = c("GEOID" = "T_GEOID")) %>%
-  write_sf("Data/Spatial/bg_totals.shp")
+bg_location_summary <- bg_locations %>% 
+  mutate(GEOID_tr = paste0(STATEFP, COUNTYFP, TRACTCE)) %>% 
+  left_join(dac_criteria, by = "GEOID_tr") %>% 
+  select(GEOID, county, dac_status, geometry) %>% 
+  mutate(dac_status = replace_na(dac_status, "not DAC status")) %>% 
+  left_join(bind_rows(bg_access, bg_bad_matched), by = c("GEOID" = "T_GEOID"))
+
+
+bg_location_summary %>%
+  select(GEOID, county, bg_sta1d, bg_sta2d,
+         nearest_sta_miles, second_nearest_sta_miles, geometry) %>% 
+  write_sf("Data/Spatial/bg_totals_20210920.shp")
+
+bg_location_summary %>% 
+  select(-county) %>% 
+  st_drop_geometry() %>% 
+  write_csv("Data/block_group_to_hydrogen_fueling_station.csv")
+
+
